ab17fd5bc64bf7c575f457be6f48adb183cc02b4
Ani Tukvadze <ani.tukvadze14@geolab.edu.ge>
2021-11-20 04:34:33 -0800

    added splice, filter, push

---
+++ app.js
@@ -0,0 +1,37 @@
+//Array.prototype.splice()
+//splice მეთორი ცვლის მასივის შიგთავსს ელემენტების ამოღებით, ჩანაცვლებით ან დამატებით.
+// let apps = ['facebook', 'instagram','twitter', 'snapchat']
+// let removed = apps.splice(2,0, 'whatsApp');
+// console.log(apps);
+
+// let animals = ['dog', 'cat', 'horse', 'bear', 'squirrel']
+// let removed = animals.splice(0, 3, 'raccoon', 'hedgehog');
+// console.log(animals);
+
+
+//Array.prototype.filter()
+//filter მეთოდი ქმნის ახალ array-ს სადაც მოცემულია ის ელემენტები რომელიც აკმაყოფილებს ახალ მოთხოვნას.
+// let numbers = [10, 15, 25, 35, 45, 55]
+// let hightNumbers = numbers.filter(function(number) {
+//     return number > 35;
+// });
+
+// console.log(hightNumbers);
+
+
+// let names = ['mari', 'nika', 'giorgi', 'elene', 'ani']
+// let someNames = names.filter(function(names) {
+//     return names.length > 4 
+// });
+// console.log(someNames);
+
+
+//Array.prototype.push()
+//push მეთოდი ამატებს ახალ ელემენტს მასივის ბოლოში.
+// let clothes = ['pants', 'blouse', 'jacket', 'sweater']
+// let total = clothes.push('t-shirt', 'coat', 'scarf');
+// console.log(clothes);
+
+// let jewellery = ['earring', 'bracelet', 'ring', 'necklace']
+// let total = jewellery.push('watch');
+// console.log(jewellery);
