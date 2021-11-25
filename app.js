57a85b41a723c6c14c0d593712e0e8f3d93abeac
Ani Tukvadze <ani.tukvadze14@geolab.edu.ge>
2021-11-25 11:46:40 -0800

    added 1,2,3 examples

--- app.js
+++ app.js
@@ -35,3 +35,96 @@
 // let jewellery = ['earring', 'bracelet', 'ring', 'necklace']
 // let total = jewellery.push('watch');
 // console.log(jewellery);
+
+// const age = 19;
+// const vaccinated = true;
+// if (age > 18 && vaccinated) {
+//     console.log('Yes');
+
+// } else {
+//     console.log('No');
+// } 
+
+
+// const age = 21;
+// const vaccinated = true;
+
+// if (age < 10) {
+//     console.log('child');
+
+// } else if (age < 20) {
+//     console.log('teen');
+
+// } else {
+//     console.log('adult');
+// } 
+
+// const age = 50;
+// const vaccinated = true;
+
+// if (age >  10 && age < 50 || vaccinated) {
+//     console.log('Yes');
+
+// } else {
+//     console.log('No');
+// } 
+
+// const age = 50;
+// const vaccinated = true;
+
+// if (age >  50 && (age < 10 || vaccinated)) {
+//     console.log('Yes');
+
+// } else {
+//     console.log('No');
+// } 
+
+// (5 >3) && (console.log('Yes'));
+
+// let numbers = [14, 2, 45, 13, 54, 66, 21, 40];
+// let min = numbers[2];
+// for (let i = 1; i < numbers.length; i++) {
+//     if (numbers[i] < min) {
+//         min = numbers[i];
+//     }
+// }
+// console.log('Min is:'+ min);
+
+// const arr = [14, 2, 45, 13, 54, 66, 21, 40];
+// const min = Math.min(...arr);
+// const max = Math.max(...arr);
+// console.log(min);
+// console.log(max);
+
+// let x = myFunction(2, 3);
+// function myFunction(a, b) {
+//     return a ** b;
+// }
+// console.log(x);
+
+
+// let x = myFunction(5,6);
+// function myFunction(a, b) {
+//     return a ** b;
+// }
+// console.log(x);
+
+
+
+// let x = (a, b) => {
+//     let result = a ** b;
+//     return result;
+// }
+
+// let result1 = x( 2 ,3)
+// console.log(result1);
+
+
+// let x = (a, b) => {
+//     let result = a ** b;
+//     return result;
+// }
+
+// let result1 = x( 5, 6)
+// console.log(result1);
+  
---
+++ main.js
@@ -0,0 +1,43 @@
+const arr = [14, 2, 45, 13, 54, 66, 21, 40];
+const min = Math.min(...arr);
+const max = Math.max(...arr);
+console.log(min);
+console.log(max);
+
+
+
+
+let x = myFunction(2, 3);
+function myFunction(a, b) {
+    return a ** b;
+}
+console.log(x);
+
+
+let x = myFunction(5, 6);
+function myFunction(a, b) {
+    return a ** b;
+}
+console.log(x);
+
+
+
+
+
+let x = (a, b) => {
+    let result = a ** b;
+    return result;
+}
+
+let result1 = x( 2 ,3)
+console.log(result1);
+
+
+let x = (a, b) => {
+    let result = a ** b;
+    return result;
+}
+
+let result1 = x( 5, 6)
+console.log(result1);
+  
