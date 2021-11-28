d3ee37973c990e49b5fa64c5a6438baa5e040959
Ani Tukvadze <ani.tukvadze14@geolab.edu.ge>
2021-11-28 09:50:00 -0800

    added a third task

--- app.js
+++ app.js
@@ -1,130 +1,38 @@
-//Array.prototype.splice()
-//splice მეთორი ცვლის მასივის შიგთავსს ელემენტების ამოღებით, ჩანაცვლებით ან დამატებით.
-// let apps = ['facebook', 'instagram','twitter', 'snapchat']
-// let removed = apps.splice(2,0, 'whatsApp');
-// console.log(apps);
+Array.prototype.splice()
+splice მეთორი ცვლის მასივის შიგთავსს ელემენტების ამოღებით, ჩანაცვლებით ან დამატებით.
+let apps = ['facebook', 'instagram','twitter', 'snapchat']
+let removed = apps.splice(2,0, 'whatsApp');
+console.log(apps);
 
-// let animals = ['dog', 'cat', 'horse', 'bear', 'squirrel']
-// let removed = animals.splice(0, 3, 'raccoon', 'hedgehog');
-// console.log(animals);
-
-
-//Array.prototype.filter()
-//filter მეთოდი ქმნის ახალ array-ს სადაც მოცემულია ის ელემენტები რომელიც აკმაყოფილებს ახალ მოთხოვნას.
-// let numbers = [10, 15, 25, 35, 45, 55]
-// let hightNumbers = numbers.filter(function(number) {
-//     return number > 35;
-// });
-
-// console.log(hightNumbers);
+let animals = ['dog', 'cat', 'horse', 'bear', 'squirrel']
+let removed = animals.splice(0, 3, 'raccoon', 'hedgehog');
+console.log(animals);
 
 
-// let names = ['mari', 'nika', 'giorgi', 'elene', 'ani']
-// let someNames = names.filter(function(names) {
-//     return names.length > 4 
-// });
-// console.log(someNames);
+Array.prototype.filter()
+filter მეთოდი ქმნის ახალ array-ს სადაც მოცემულია ის ელემენტები რომელიც აკმაყოფილებს ახალ მოთხოვნას.
+let numbers = [10, 15, 25, 35, 45, 55]
+let hightNumbers = numbers.filter(function(number) {
+    return number > 35;
+});
+
+console.log(hightNumbers);
 
 
-//Array.prototype.push()
-//push მეთოდი ამატებს ახალ ელემენტს მასივის ბოლოში.
-// let clothes = ['pants', 'blouse', 'jacket', 'sweater']
-// let total = clothes.push('t-shirt', 'coat', 'scarf');
-// console.log(clothes);
-
-// let jewellery = ['earring', 'bracelet', 'ring', 'necklace']
-// let total = jewellery.push('watch');
-// console.log(jewellery);
-
-// const age = 19;
-// const vaccinated = true;
-// if (age > 18 && vaccinated) {
-//     console.log('Yes');
-
-// } else {
-//     console.log('No');
-// } 
+let names = ['mari', 'nika', 'giorgi', 'elene', 'ani']
+let someNames = names.filter(function(names) {
+    return names.length > 4 
+});
+console.log(someNames);
 
 
-// const age = 21;
-// const vaccinated = true;
+Array.prototype.push()
+push მეთოდი ამატებს ახალ ელემენტს მასივის ბოლოში.
+let clothes = ['pants', 'blouse', 'jacket', 'sweater']
+let total = clothes.push('t-shirt', 'coat', 'scarf');
+console.log(clothes);
 
-// if (age < 10) {
-//     console.log('child');
+let jewellery = ['earring', 'bracelet', 'ring', 'necklace']
+let total = jewellery.push('watch');
+console.log(jewellery);
 
-// } else if (age < 20) {
-//     console.log('teen');
-
-// } else {
-//     console.log('adult');
-// } 
-
-// const age = 50;
-// const vaccinated = true;
-
-// if (age >  10 && age < 50 || vaccinated) {
-//     console.log('Yes');
-
-// } else {
-//     console.log('No');
-// } 
-
-// const age = 50;
-// const vaccinated = true;
-
-// if (age >  50 && (age < 10 || vaccinated)) {
-//     console.log('Yes');
-
-// } else {
-//     console.log('No');
-// } 
-
-// (5 >3) && (console.log('Yes'));
-
-// let numbers = [14, 2, 45, 13, 54, 66, 21, 40];
-// let min = numbers[2];
-// for (let i = 1; i < numbers.length; i++) {
-//     if (numbers[i] < min) {
-//         min = numbers[i];
-//     }
-// }
-// console.log('Min is:'+ min);
-
-// const arr = [14, 2, 45, 13, 54, 66, 21, 40];
-// const min = Math.min(...arr);
-// const max = Math.max(...arr);
-// console.log(min);
-// console.log(max);
-
-// let x = myFunction(2, 3);
-// function myFunction(a, b) {
-//     return a ** b;
-// }
-// console.log(x);
-
-
-// let x = myFunction(5,6);
-// function myFunction(a, b) {
-//     return a ** b;
-// }
-// console.log(x);
-
-
-
-// let x = (a, b) => {
-//     let result = a ** b;
-//     return result;
-// }
-
-// let result1 = x( 2 ,3)
-// console.log(result1);
-
-
-// let x = (a, b) => {
-//     let result = a ** b;
-//     return result;
-// }
-
-// let result1 = x( 5, 6)
-// console.log(result1);
-  
--- main.js
+++ main.js
@@ -41,3 +41,54 @@ let x = (a, b) => {
 let result1 = x( 5, 6)
 console.log(result1);
   
+
+
+const arr = [ 4, 5, 11, 2, 9, 99, 1092, 1];
+const sorted = arr.slice().sort((a,b)=> a-b);
+console.log(sorted);
+
+const numArray = [ 4, 5, 11, 2, 9, 99, 1092, 1];
+numArray.sort((a, b) => Math.sign(a - b))
+console.log(numArray);
+
+
+
+let arr = [ 4, 5, 11, 2, 9, 99, 1092, 1];
+let evens = arr.filter(function(x) {
+    if ( x % 2 === 0 || x === 0) {
+        return x;
+    }
+
+});
+console.log(evens);
+
+
+
+
+let arr = [ 4, 5, 11, 2, 9, 99, 1092, 1];
+let filtered = arr.filter(function(element, index, array){
+    return (index % 2 === 0);
+});
+
+
+
+
+
+let arr = [4,2,1092];
+let reducer = (previousValue, currentValue) => previousValue * currentValue;
+console.log(arr.reduce(reducer));
+
+
+
+
+
+let arr = [ 'HELLO', 'WORLD', 'JS', 'GEOLAB'];
+let capitalize = arr.map(arr => arr.charAt(0).toUpperCase() + arr.slice(1).toLowerCase());
+console.log(capitalize);
+
+
+let newArr = [ 'HELLO', 'WORLD', 'JS', 'GEOLAB'];
+for (let i = 0; i < newArr.length; i++) {
+    newArr[i] = newArr[i].charAt(0).toUpperCase() + newArr[i].slice(1).toLowerCase();
+};
+console.log(newArr);
